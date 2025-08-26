d91da0853ab9b43cb238f78d14f49433a52ca74c
AuTeLipi <lipipgm@gmail.com>
2025-08-26 18:58:42 +0530

    26th Aug

---
+++ src/test/java/com/LipiAutomation/Ex02_Selenium_Basics/TestSelenium10_Change_Driver.java
@@ -0,0 +1,14 @@
+package com.LipiAutomation.Ex02_Selenium_Basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestSelenium10_Change_Driver {
+
+    public static void main(String[] args) {
+        WebDriver driver1 = new ChromeDriver();
+        driver1 = new FirefoxDriver();
+    }
+
+}
---
+++ src/test/java/com/LipiAutomation/Ex02_Selenium_Basics/TestSelenium11_Navigations.java
@@ -0,0 +1,30 @@
+package com.LipiAutomation.Ex02_Selenium_Basics;
+
+import jdk.jfr.Description;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium11_Navigations {
+
+    @Description("Open the URL")
+    @Test
+    public void test_Selenium01(){
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://google.com"); // 95%
+
+        //  Use Navigation Commands
+        // driver.get("url") -> Navigate to a URL
+
+        //  navigate.to()
+
+        // Use navigation methods
+        driver.navigate().to("https://bing.com");
+        driver.navigate().back();
+        driver.navigate().refresh();
+        driver.navigate().forward();
+
+    }
+
+}
