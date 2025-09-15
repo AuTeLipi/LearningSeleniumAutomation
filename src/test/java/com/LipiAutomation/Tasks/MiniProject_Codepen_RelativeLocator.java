fd2e0189ced4374dfb4acf2e356aaa469c0c4e28
AuTeLipi <lipipgm@gmail.com>
2025-09-15 22:30:54 +0530

    15th Sept

--- src/test/java/com/LipiAutomation/Tasks/MiniProject_Codepen_RelativeLocator.java
+++ src/test/java/com/LipiAutomation/Tasks/MiniProject_Codepen_RelativeLocator.java
@@ -1,6 +1,5 @@
 package com.LipiAutomation.Tasks;
 
-import com.LipiAutomation.Ex09_WaitHelper.WaitHelpers;
 import com.LipiAutomation.utils.CommonToAll;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,52 +8,51 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.openqa.selenium.support.locators.RelativeLocator.with;
 
 public class MiniProject_Codepen_RelativeLocator extends CommonToAll {
 
     @Test
-    public void test_Codepen_RelativeLocator() {
+    public void RelativeLocator() {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://codepen.io/AbdullahSajjad/full/LYGVRgK");
         driver.manage().window().maximize();
 
-        Assert.assertEquals(driver.getCurrentUrl(), "https://codepen.io/AbdullahSajjad/full/LYGVRgK");
-
-        //Submit button is in iframe
         driver.switchTo().frame("result");
+
         driver.findElement(By.xpath("//form[@id='form']/button")).click();
 
-        new WaitHelpers().waitJVM(2000);
+        // username input and error message
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement usernameError = driver.findElement(with(By.tagName("small")).below(usernameInput));
+        String usernameErrorMsg = usernameError.getText();
+        System.out.println("Username error: " + usernameErrorMsg);
+        Assert.assertEquals(usernameErrorMsg, "Username must be at least 3 characters");
 
-        List<WebElement> locators = driver.findElements(By.xpath("//div[@class='form-control error']"));
+        // email input and error message
+        WebElement emailInput = driver.findElement(By.id("email"));
+        WebElement emailError = driver.findElement(with(By.tagName("small")).below(emailInput));
+        String emailErrorMsg = emailError.getText();
+        System.out.println("Email error: " + emailErrorMsg);
+        Assert.assertEquals(emailErrorMsg, "Email is not invalid");
 
-        List<String> expectedErrors = Arrays.asList(
-                "Username must be at least 3 characters",
-                "Email is not invalid",
-                "Password must be at least 6 characters",
-                "Password2 is required"
-        );
+        // password input and error message
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        WebElement passwordError = driver.findElement(with(By.tagName("small")).below(passwordInput));
+        String passwordErrorMsg = passwordError.getText();
+        System.out.println("Password error: " + passwordErrorMsg);
+        Assert.assertEquals(passwordErrorMsg, "Password must be at least 6 characters");
 
-        for (int i = 0; i < locators.size(); i++) {
-            WebElement smallElement = locators.get(i).findElement(By.tagName("small"));
-            String actualMsg = smallElement.getText();
-            String expectedMsg = expectedErrors.get(i);
-
-            System.out.println("----------------------------------------------");
-            System.out.println("Actual: " + actualMsg);
-            System.out.println("Expected: " + expectedMsg);
-
-            Assert.assertEquals(actualMsg, expectedMsg, "Mismatch at index " + i);
-        }
-
+        // confirm password input and error message
+        WebElement confirmPasswordInput = driver.findElement(By.id("password2"));
+        WebElement confirmPasswordError = driver.findElement(with(By.tagName("small")).below(confirmPasswordInput));
+        String confirmPasswordErrorMsg = confirmPasswordError.getText();
+        System.out.println("Confirm password error: " + confirmPasswordErrorMsg);
+        Assert.assertEquals(confirmPasswordErrorMsg, "Password2 is required");
 
         closeBrowser(driver);
+
 
     }
 }
