b79cdd91114448fffa342f7fa4c33b49584ed25b
AuTeLipi <lipipgm@gmail.com>
2025-09-16 22:42:34 +0530

    16th Sept

--- src/test/java/com/LipiAutomation/Tasks/MiniProject_VWO_DataProvider.java
+++ src/test/java/com/LipiAutomation/Tasks/MiniProject_VWO_DataProvider.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -17,8 +18,9 @@ import java.time.Duration;
 public class MiniProject_VWO_DataProvider extends CommonToAll {
 
     @Test(dataProvider = "getData")
-    public void test_VWOLoginDataDriven(String email, String password) {
+    public void test_VWOLoginDataDriven_NegativeTC(String email, String password) {
 
+        //Using Data Driven Testing
         WebDriver driver = new ChromeDriver();
         driver.get("https://app.vwo.com/#/login");
         driver.manage().window().maximize();
@@ -40,12 +42,14 @@ public class MiniProject_VWO_DataProvider extends CommonToAll {
         WebElement Button = driver.findElement(By.xpath("//button[@id='js-login-btn']"));
         Button.click();
 
-        new WaitHelpers().waitJVM(9000);
+        new WaitHelpers().waitJVM(3000);
 
-        System.out.println(driver.getTitle());
+        WebElement ErrorMsg = driver.findElement(By.xpath("//div[text()='Your email, password, IP address or location did not match']"));
+        System.out.println("Error Message: " +ErrorMsg);
 
-        WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(3));
-        wait1.until(ExpectedConditions.titleIs("Dashboard"));
+        Assert.assertEquals(ErrorMsg.getText(), "Your email, password, IP address or location did not match");
+
+        closeBrowser(driver);
 
     }
 
@@ -54,5 +58,41 @@ public class MiniProject_VWO_DataProvider extends CommonToAll {
         // READ THE DATA FROM THE EXCEL FILE
         // GIVE THEM IN THE 2D ARRAY
         return UtilExcel.getTestDataFromExcel("sheet1");
+    }
+
+    @Test()
+    public void test_VWOLoginDataDriven_PositiveTC() {
+
+        //Without using Data Driven Testing
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://app.vwo.com/#/login");
+        driver.manage().window().maximize();
+
+        System.out.println(driver.getTitle());
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.titleIs("Login - VWO"));
+
+        WebElement usernameInputBox = driver.findElement(By.id("login-username"));
+        usernameInputBox.sendKeys("contact+aug@thetestingacademy.com");
+
+        WebElement passwordInputBox = driver.findElement(By.id("login-password"));
+        passwordInputBox.sendKeys("TtxkgQ!s$rJBk85");
+
+        WebElement RememberMe = driver.findElement(By.xpath("(//*[name()='svg' and contains(@class, 'checkbox-radio-icon')])[1]"));
+        RememberMe.click();
+
+        WebElement Button = driver.findElement(By.xpath("//button[@id='js-login-btn']"));
+        Button.click();
+
+        new WaitHelpers().waitJVM(9000);
+
+        System.out.println(driver.getTitle());
+
+        WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait1.until(ExpectedConditions.titleIs("Dashboard"));
+
+        closeBrowser(driver);
+
     }
 }
